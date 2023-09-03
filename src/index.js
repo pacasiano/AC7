2e59b6c58319f8ff35b4d4aba2858ee5a262d119
Peter <pacasiano@addu.edu.ph>
2023-09-03 12:12:53 +0800

    minor changes on basename

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/AC7">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
