6d2e05aa201501211613bdf8f4591fbb71b0915b
anthonyyp <yappanthony22@gmail.com>
2023-09-19 22:04:02 +0800

    create item.js file api route (addToCart)

---
+++ server/routes/item.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
+const mysql = require('mysql2');
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'whatamIdoing332', //enter your own password
+    database: 'ac7_database'
+});
+
+//parse/extract data from a <form>
+app.use(express.urlencoded( { extended: true } ));
+
+app.post('/api/login', (req, res) => {
+    const {username, password} = req.body;
+
+    let q = `SELECT account_id, username, password FROM account WHERE username = '${username}'`;
+    connection.query(q, function(error, results, fields) {
+        if (error) throw error;
+        //results is an array of objects, results[0] assumes that we want the first row which should be valid if username is unique
+        if (results.length > 0) { //at least 1 row is returned from the query (username exists)
+            try {
+                const {account_id, username: dbUsername, password: dbPassword} = results[0]; //need to add some error handling
+                console.log("DB user: " + dbUsername);
+                console.log("DB pw: " + dbPassword);
+                if (password === dbPassword) {
+                    //log the user in
+                    res.cookie('account_id', `${account_id}`, {maxAge: 3600000})
+                    res.redirect('http://localhost:3000/AC7/home');
+                }
+            }
+            catch (err) {
+                console.log(err);
+            }
+        }
+        else {
+            res.send("Username doesn't exist!")
+        }
+        // res.json(results); //returns an array of obj literals in JSON format, each obj literal is a row from users table
+        // connection.end()
+    });
+});
+
+module.exports = app;
