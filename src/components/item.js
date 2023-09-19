ff351ce34a4cb9edacac12d8e697067d27b67333
anthonyyp <yappanthony22@gmail.com>
2023-09-19 17:04:12 +0800

    add form tag and input tag in item.js

--- src/components/item.js
+++ src/components/item.js
@@ -6,27 +6,29 @@ import "../App.css";
 function Item(props) {
 
   return (
-    <div className="item shadow-md rounded-md">
-      
-      <div className="flex flex-col justify-center items-center gap-1 w-auto">
-        <Link to="/product">
-        <img className="object-cover h-15 px-3 pt-3 rounded-md aspect-auto" src={Item1} alt="Item1" />
-        </Link>
-        <div className="flex flex-col px-2 justify-start gap-2 lg:w-52 w-40">
-          <div className="flex flex-col pl-3">
-            <div className="flex justify-start m-0 text-xl font-extrabold">{props.product_obj.name}</div>
-            <div className="flex m-0 justify-start text-xs font-semibold">${props.product_obj.price}</div>
-            <div className="flex m-0 justify-start text-xs">{props.product_obj.description}</div>
-            {/* props is the parameter name here. product_obj is the parameter name we set in itemSlider.js 
-            which contains the columns we queried from routes/products.js. 
+    <form action="/api/cart" method="POST">
+      <div className="item shadow-md rounded-md">
+        
+        <div className="flex flex-col justify-center items-center gap-1 w-auto">
+          <Link to="/product">
+          <img className="object-cover h-15 px-3 pt-3 rounded-md aspect-auto" src={Item1} alt="Item1" />
+          </Link>
+          <div className="flex flex-col px-2 justify-start gap-2 lg:w-52 w-40">
+            <div className="flex flex-col pl-3">
+              <input className="flex justify-start m-0 text-xl font-extrabold" value={props.product_obj.name} readOnly={true}/>
+              <input className="flex m-0 justify-start text-xs font-semibold" value={`$${props.product_obj.price}`} readOnly={true} />
+              <input className="flex m-0 justify-start text-xs" value={props.product_obj.description} readOnly={true}/>
+              {/* props is the parameter name here. product_obj is the parameter name we set in itemSlider.js 
+              which contains the columns we queried from routes/products.js. 
             Basically, 'props' contains a nested obj. We access the nested obj using 'product_obj'*/}
-          </div>
-          <div className="flex justify-center py-2">
-            <button className="flex items-center justify-center w-11/12 mb-2 h-6 text-xs font-bold text-white bg-black rounded-md">Add to Cart</button>
+            </div>
+            <div className="flex justify-center py-2">
+              <button className="flex items-center justify-center w-11/12 mb-2 h-6 text-xs font-bold text-white bg-black rounded-md">Add to Cart</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
 
