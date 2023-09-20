5271c36704c275d28d342b264176d459997ca7fa
anthonyyp <yappanthony22@gmail.com>
2023-09-21 06:12:35 +0800

    finish item.js api route (addToCart)

--- server/routes/item.js
+++ server/routes/item.js
@@ -15,58 +15,50 @@ const connection = mysql.createConnection({
     database: 'ac7_database'
 });
 
-// async function getProductID(product_name) {
-//     const connection = await mysql.createConnection({
-//         host: 'localhost',
-//         user: 'root',
-//         password: 'whatamIdoing332', //enter your own password
-//         database: 'ac7_database'
-//     });
-
-//     try {
-//         const q = `SELECT product_id FROM product WHERE product.name = '${product_name}'`;
-//         const [results, fields] = await connection.query(q);
-//         console.log("Results: " + results);
-
-//         if (results.length > 0) {
-//             //results is an array of objects, results[0] assumes that we want the first row
-//             //at least 1 row is returned from the query (product exists)
-//              return results[0].product_id; 
-//         } 
-//         else {
-//             throw new Error("Product doesn't exist!");
-//         }
-//       } 
-//       catch (error) {
-//           console.error('Error:', error);
-//           throw error;
-//       } 
-//       finally {
-//           await connection.end();
-//       }
-// }
-
 app.post('/api/item', (req, res) => {
     const {username, password} = req.body;
     //instead of req.headers.cookie which returns a string, we can use the cookie parser 'req.cookies' to return an object
     const {account_id} = req.cookies;
-    const {product_id, product_name, product_price} = req.body;
-    // console.log(req.cookies);
-    // console.log(req.body);
+    let {product_id, product_name, product_price} = req.body;
+    product_price = product_price.replace('$', ''); //the value returned from client-side has a dollar sign, so we remove it
 
-    let q = ""
+    let q = `SELECT sale_id FROM sale WHERE account_id = '${account_id}' AND sale_status = 'in progress'`;
     connection.query(q, function(err, results) {
-
-    })
+        if (err) {
+            console.error("Error querying the database:", err);
+            // Handle the error as needed, e.g., return an error response or throw an exception.
+            // You may want to end the request or provide a meaningful response to the client.
+            res.status(500).json({ error: "Database query error" });
+            return; // Exit the function early to prevent further execution
+        }
     
-    // res.json(results); //returns an array of obj literals in JSON format, each obj literal is a row from users table
+        if (results.length === 0) {
+            console.error("No results found for the query.");
+            // Handle the case where no results were found.
+            // You may want to return an appropriate response to the client.
+            res.status(404).json({ error: "No results found" });
+            return; // Exit the function early to prevent further execution
+        }
+    
+        const { sale_id } = results[0];
+    
+        let q2 = `INSERT INTO sale_item(sale_id, account_id, product_id, price) VALUES (${sale_id}, ${account_id}, ${product_id}, '${product_price}')`;
+        connection.query(q2, function(err, results) {
+            if (err) {
+                console.error("Error inserting into the database:", err);
+                // Handle the error as needed, e.g., return an error response or throw an exception.
+                // You may want to end the request or provide a meaningful response to the client.
+                res.status(500).json({ error: "Database insert error" });
+                return; // Exit the function early to prevent further execution
+            }
+    
+            console.log("Insert successful:", results);
+            res.redirect('http://localhost:3000/AC7/cart');
+        });
+    });
+    
+    
     // connection.end()
-    // res.redirect('http://localhost:3000/AC7/cart');
-    // console.log("Product ID: " + product_id);
-    // const q2 = ``;
-    // connection.query(q2, function(err, results) {
-
-    // })
 
     //need to query the sale_id of this user (use their account_id)
     //once you have the sale_id, create a sale_item entry 
