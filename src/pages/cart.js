ef352c89b42f5889a6613c9d6b92a0f74e2d93e0
anthonyyp <yappanthony22@gmail.com>
2023-10-03 00:19:44 +0800

    fix cookie bug in pages/cart.js

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -8,7 +8,18 @@ import CartItem from "../components/cartItem";
 function Cart() {
 
   const cookie = document.cookie;
-  const accountId = cookie.slice(cookie.indexOf('=')+1);
+  function getAcctIdFromCookie (cookieStr) {
+      //document.cookie is a string. We use .split() to convert it to an array with each cookie being an element
+      const cookiesArray = cookieStr.split(';');
+      for(let i = 0; i < cookiesArray.length; i++) {
+          if (cookiesArray[i].indexOf('account_id') > 0) {
+              //find the cookie with 'account_id' substring
+              const id = cookiesArray[i].replace('account_id=', '').trim();
+              return id;
+          }
+      }
+  }
+  const accountId = getAcctIdFromCookie(cookie);
 
   const [items, setItems] = useState([]);
 
