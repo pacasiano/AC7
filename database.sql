fae33b35bff9774f471dc5e85576200fd6109d43
anthonyyp <yappanthony22@gmail.com>
2023-11-09 15:43:34 +0800

    redirect users to the proper page based on account_type upon login

--- database.sql
+++ database.sql
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS employee (
     employee_id BIGINT UNSIGNED AUTO_INCREMENT,
     account_id BIGINT UNSIGNED NOT NULL,
     first_name VARCHAR(255) NOT NULL,
-    middle_name VARCHAR(255) NOT NULL,
+    middle_name VARCHAR(255),
     last_name VARCHAR(255) NOT NULL,
     position VARCHAR(255) NOT NULL,
     emp_status ENUM('active', 'inactive') NOT NULL DEFAULT 'active', -- status is a reserved word
@@ -192,7 +192,8 @@ VALUES ('boywonder1', 'gothamsavior42', 'customer'),
 ('starfirealien', 'tamaranlove17', 'customer'),
 ('shapeshiftpro', 'greengorillabanana', 'customer'),
 ('darksorceress', 'azarathrising99', 'customer'),
-('obvictoriano', 'adduislife', 'customer'); 
+('obvictoriano', 'adduislife', 'customer'),
+('obet', 'iluvmen', 'employee'); 
 
 INSERT INTO customer(account_id, first_name, middle_name, last_name, contact_info, email) 
 VALUES (1, 'Dick', 'Robin', 'Grayson', '123-4567', 'robin@titans.net'),
@@ -200,6 +201,9 @@ VALUES (1, 'Dick', 'Robin', 'Grayson', '123-4567', 'robin@titans.net'),
 (3, 'Garfield', 'Beastboy', 'Logan', '789-0123', 'beastboy@animalkingdom.org'),
 (4, 'Rachel', 'Raven', 'Roth', '321-6789', 'raven@darknessrealm.net'),
 (5, 'Oneil', NULL, 'Victoriano', '888-8888', 'obvictoriano@addu.edu.ph');
+
+INSERT INTO employee(account_id, first_name, middle_name, last_name, position, emp_status, contact_info)
+VALUES (6, 'obet', NULL, 'dohsgit', 'Employee', 'active', '333-4233');
 
 INSERT INTO address(customer_id, name, city, zip_code, barangay, province, street) 
 VALUES(1, 'Secret Hideout', 'Gotham', '123', 'Barangay 10', 'NA', '26 street'),
--- server/routes/login.js
+++ server/routes/login.js
@@ -18,26 +18,31 @@ router.post('/', (req, res) => {
     console.log(req.body)
     const {username, password} = req.body;
     try {
-        console.log('Receieved username: ' + req.body.username)
+        console.log('Receieved username: ' + username)
     }
     catch (err) {
         console.log(err)
     }
 
-    let q = `SELECT account_id, username, password FROM account WHERE username = '${username}'`;
+    let q = `SELECT account_id, username, password, account_type FROM account WHERE username = '${username}'`;
     connection.query(q, function(error, results, fields) {
         if (error) throw error;
         //results is an array of objects, results[0] assumes that we want the first row which should be valid if username is unique
         if (results.length > 0) { //at least 1 row is returned from the query (username exists)
             try {
-                const {account_id, username: dbUsername, password: dbPassword} = results[0]; //need to add some error handling
+                const {account_id, username: dbUsername, password: dbPassword, account_type} = results[0]; //need to add some error handling
                 console.log("DB user: " + dbUsername);
                 console.log("DB pw: " + dbPassword);
                 if (password === dbPassword) {
                     const expirationDate = new Date(); //create a new date
                     expirationDate.setDate(expirationDate.getDate() + 30); //set the date to the current time when user logs in then add 30 days to it
                     res.cookie('account_id', `${account_id}`, {expires: expirationDate}) //cookie expires in 30 days
-                    res.redirect('http://localhost:3000/AC7/home');
+                    if (account_type === 'customer') {
+                        res.redirect('http://localhost:3000/AC7/home');
+                    }
+                    else {
+                        res.redirect('http://localhost:3000/AC7/admin');
+                    }
                 }
             }
             catch (err) {
