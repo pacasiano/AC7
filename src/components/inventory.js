0a466c6e1e5b0ebb16dc82863326d95a91548287
pacasiano <pacasiano@addu.edu.ph>
2023-11-22 22:58:51 +0800

    Updates on inventory and order components

--- src/components/inventory.js
+++ src/components/inventory.js
@@ -5,8 +5,8 @@ import Select from "react-select";
 import { IoWarning } from "react-icons/io5";
 
 export default function Inventory() {
-  const [addItem, showAddItem] = useState(false);
-  const [editItem, showEditItem] = useState(false);
+
+  
 
   const { setPage } = useContext(myContext);
   const [products, setProducts] = useState([]);
@@ -78,22 +78,15 @@ export default function Inventory() {
                   <th className="sticky top-0 bg-gray-400 text-sm font-semibold border p-2 text-white">Name</th>
                   <th className="sticky top-0 bg-gray-400 text-sm font-semibold border p-2 text-white">Description</th>
                   <th className="sticky top-0 bg-gray-400 text-sm font-semibold border p-2 text-white">Category</th>
-                  <th className="sticky top-0 bg-gray-400 text-sm font-semibold border p-2 text-white">Price</th>
+                  {/* <th className="sticky top-0 bg-gray-400 text-sm font-semibold border p-2 text-white">Price</th> */}
                   <th className="sticky top-0 bg-gray-400 text-sm font-semibold border p-2 text-white">Threshold</th>
                   <th className="sticky top-0 bg-gray-400 text-sm font-semibold border p-2 text-white">Quantity</th>
+                  <th className="sticky top-0 bg-gray-400 text-sm font-semibold border p-2 text-white">Actions</th>
                 </tr>
               </thead>
               <tbody>
                 {filteredProducts.map((product) => (
-                  <tr className={`${product.quantity <= product.threshold ? "bg-gray-300" : "bg-gray-300"} h-12`} key={product.product_id}>
-                    <td className="text-sm font-semibold border p-2">{product.product_id}</td>
-                    <td className="text-sm font-semibold border p-2">{product.name}</td>
-                    <td className="text-sm font-semibold border p-2">{product.description}</td>
-                    <td className="text-sm font-semibold border p-2">{product.category}</td>
-                    <td className="text-sm font-semibold border p-2">&#x20B1;{product.price}</td>
-                    <td className="text-sm font-semibold border p-2">{product.threshold}</td>
-                    <td className={`${product.quantity <= product.threshold && "text-orange-500"} font-semibold text-sm  border p-2`}><div className="flex flex-row items-center gap-1">{product.quantity} {product.quantity <= product.threshold ? <IoWarning className="text-2xl font-bold" /> : ""}</div></td>
-                  </tr>
+                <InventoryCard key={product.product_id} product={product} />
                 ))}
               </tbody>
             </table>
@@ -101,5 +94,65 @@ export default function Inventory() {
         </div>
       </div>
     </div>
+  );
+}
+
+function InventoryCard({ product }) {
+
+  const [extend, setExtend] = useState(false);
+
+  function extendHandler() {
+    setExtend(!extend);
+  }
+
+  return (
+    <>
+    <tr className={`${product.quantity <= product.threshold ? "bg-gray-300" : "bg-gray-300"} h-12`} key={product.product_id}>
+      <td className="text-sm font-semibold border p-2">{product.product_id}</td>
+      <td className="text-sm font-semibold border p-2">{product.name}</td>
+      <td className="text-sm font-semibold border p-2">{product.description}</td>
+      <td className="text-sm font-semibold border p-2">{product.category}</td>
+      {/* <td className="text-sm font-semibold border p-2">&#x20B1;{product.price}</td> */}
+      <td className="text-sm font-semibold border p-2">{product.threshold}</td>
+      <td className={`${product.quantity <= product.threshold && "text-orange-500"} font-semibold text-sm  border p-2`}><div className="flex flex-row items-center gap-1">{product.quantity} {product.quantity <= product.threshold ? <IoWarning className="text-2xl font-bold" /> : ""}</div></td>
+      {extend ? (
+        <td className="text-sm font-semibold border px-2 w-20 text-white"><button onClick={extendHandler} className="bg-blue-500 p-2 w-full rounded-md">Close</button></td>  
+      ) : (
+        <td className="text-sm font-semibold border px-2 w-20 text-white"><button onClick={extendHandler} className="bg-blue-500 p-2 w-full rounded-md">Extend</button></td>
+      )}
+    </tr>
+    {extend && (
+    <tr>
+      <td className="text-sm text-center bg-slate-100" colSpan={7}>
+      
+      <table className="w-full">
+        <thead>
+          <tr className="bg-slate-200">
+            <th className="text-sm font-semibold p-2 text-neutral-800">Batch no.</th>
+            <th className="text-sm font-semibold p-2 text-neutral-800">Price</th>
+            <th className="text-sm font-semibold p-2 text-neutral-800">Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* repeat this shi */}
+          {/* {batch.map((batch) => ( */}
+            <tr className="h-8 border-t border-white">
+              <td className="text-sm font-normal">69696969</td>
+              <td className="text-sm font-normal">&#x20B1; 69</td>
+              <td className="text-sm font-normal">69</td>
+            </tr>
+            <tr className="h-8 border-t border-white">
+              <td className="text-sm font-normal">69696969</td>
+              <td className="text-sm font-normal">&#x20B1; 69</td>
+              <td className="text-sm font-normal">69</td>
+            </tr>
+          {/* ))} */}
+        </tbody>
+      </table>
+
+      </td>
+    </tr>
+    )}
+    </>
   );
 }
