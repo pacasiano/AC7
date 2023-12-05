947400b32867d8be738507f3343cb64f0fb10414
anthonyyp <yappanthony22@gmail.com>
2023-12-05 17:54:02 +0800

    change shipment to shipped_sale

--- server/routes/shipment.js
+++ server/routes/shipment.js
@@ -24,47 +24,8 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/:id', (req, res) => {
-    const {id: sale_id} = req.params;
-    const {tracknum, courier, payment, account_id} = req.body;
-    //Insert into shipped_sale the details of shipment
-    const q1 = `INSERT INTO shipped_sale SET sale_id = ${sale_id}, address_id = (SELECT address_id FROM sale WHERE sale_id = ${sale_id}), ` +
-                `employee_id = (SELECT employee_id FROM employee WHERE account_id = ${account_id}), tracking_number = '${tracknum}', ` +
-                `courier = '${courier}', payment = ${payment}`;
-    connection.query(q1, (err, results) => {
-        if (err) {
-            console.error(err)
-        }
-        else {
-            console.log('[Shipped sale] Query 1: Goods')
-        }
-    })
-
-    //Change sale_status to shipped
-    const q2 = `UPDATE sale SET sale_status = 'shipped' WHERE sale_id = ${sale_id}`
-    connection.query(q2, (err, results) => {
-        if (err) {
-            console.error(err)
-        }
-        else {
-            console.log('[Shipped sale] Query 2: Goods')
-            res.json({message: 'Goods'})
-        }
-    })
-})
 router.get('/', (req, res) => {
-    let q = `SELECT tracking_number FROM shipment`;
-    connection.query(q, function (err, results) {
-        if (err) {
-            console.log(err.message);
-        }
-        // console.log(results);
-        res.json(results);
-    });
-});
-
-router.get('/', (req, res) => {
-    let q = `SELECT tracking_number FROM shipment`;
+    let q = `SELECT tracking_number FROM shipped_sale`;
     connection.query(q, function (err, results) {
         if (err) {
             console.log(err.message);
